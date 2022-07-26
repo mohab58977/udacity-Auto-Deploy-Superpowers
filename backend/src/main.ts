650d92bf13d75b2321108eba7475b2e0baea74f9
mohab58977 <105084710+mohab58977@users.noreply.github.com>
2022-07-26 13:41:04 +0200

    build-stage-fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -30,6 +30,7 @@ async function bootstrap() {
     .setBasePath(apiVersionPrefix)
     .addBearerAuth()// here is an intentional compile error. Remove the "x" and the backend should compile.
     .build();
+  
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(`api/${apiVersionPrefix}`, app, document);
   const config: ConfigService = app.get('ConfigService');
